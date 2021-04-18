e1d50e1cdb7dfc20d804b4c51368d1a8ad9fcdb4
ghazi_alig <mohammadghaziraza@gmail.com>
2021-04-18 10:42:46 +0530

    Comments added and the non integral input bug is fixed

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Create objects of Scanner and Password Class
         Scanner sc = new Scanner(System.in);
         Password passwordGenerator = new Password();
 
-        System.out.print("\nEnter the length of password to generate:\t");
-        int pswd_lnth = sc.nextInt();
+        //Declare identifier for password length and taking taking the input from user
+        //If the input is not integral, then an exception is thrown
+        int password_length ;
+        while (true) {
+            System.out.print("\nEnter the length of password to generate:\t");
+            String input = sc.next();
 
-        int[] arr = passwordGenerator.PasswordGenerator(pswd_lnth);
+            try {
+                password_length = Integer.parseInt(input);
+                break;
+            }
+            catch (NumberFormatException e) {
+                System.out.print("\nEnter an integer only");
+            }
+        }
 
-        System.out.println("\n\nThe randomly generated '" + pswd_lnth + "' digit password is:\n");
+        //Calling the method from Password class and
+        // getting the generated password from there with return statement
+        int[] arr = passwordGenerator.PasswordGenerator(password_length);
+
+        //The password method generates ASCII values
+        // so we need to convert those into char before printing them.
+        System.out.println("\n\nThe randomly generated '" + password_length + "' digit password is:\n");
         for (int value : arr) {
             char digit = (char) value;
             System.out.print(digit);
         }
         System.out.println();
 
-        //Do it again
-
+        //Ask the user if he wishes to do it again
         System.out.println("\n\nDo you wish to generate another password?\nEnter 'Y' if yes, any other character if no.");
         char ch = sc.next().charAt(0);
         if (ch == ('y' | 'Y')) {
--- src/Utilities/Password.java
+++ src/Utilities/Password.java
@@ -4,6 +4,10 @@ import java.util.Random;
 
 public class Password {
     public int[] PasswordGenerator(int pswd_lnth) {
+
+        //With the help of Random method, we will generate random ASCII values
+        // in the range of 33 (which is !) and 126 (which is ~).
+        // Outer bound of the range is not included when generating random numbers.
         Random random = new Random();
         int[] password = new int[pswd_lnth];
         for (int i=0; i<pswd_lnth; i++) {
